41d6652ef43ffc6eb6725d8b6663c9191d7d3f93
Enchan <enchant0130@gmail.com>
2021-12-01 09:21:18 +0900

    [Add #7] Access.c (for separate access and search process)

--- src/Field/Search.c
+++ src/Field/Search.c
@@ -1,27 +1,8 @@
 //
-// 探索
+// フィールドを探索
 //
 
 #include "Field.h"
-
-unsigned char* getDataAt(Field* f, Point p) {
-    // 範囲外ならNULLを返す
-    if (p.x >= f->width || p.y >= f->height) {
-        return NULL;
-    }
-
-    return &(f->field[p.y * f->width + p.x]);
-}
-
-void setDataAt(Field* f, Point p, unsigned char value) {
-    // 範囲外なら戻る
-    if (p.x >= f->width || p.y >= f->height) {
-        return;
-    }
-
-    // 位置を特定して値を更新
-    f->field[p.y * f->width + p.x] = value;
-}
 
 int search(Field* f, Point p, char vx, char vy, unsigned char* buf) {
     Point current = p;  // 現在読んでいる場所
